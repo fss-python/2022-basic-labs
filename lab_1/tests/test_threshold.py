40bf827b6bccb4d6b147006d5825a6061f3d2f4b
Alexander Demidovskij <alexander.demidovskij@intel.com>
2022-02-14 17:42:29 +0300

    simpler test

--- lab_1/tests/test_threshold.py
+++ lab_1/tests/test_threshold.py
@@ -5,4 +5,4 @@ from lab_1.main import calculate_threshold
 
 class ThresholdTest(unittest.TestCase):
     def test_1(self):
-        self.assertAlmostEqual(calculate_threshold([1,2,3]), 2.4, places=2)
+        self.assertIsNone(calculate_threshold([1,2,3]))
