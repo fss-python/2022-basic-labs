ebbdf4042e8e8b00918a60d8ccc688ab14cc5fef
Julia <Julia@MacBook-Julia.local>
2022-02-22 16:23:11 +0300

    add file

---
+++ main.py
@@ -0,0 +1,32 @@
+a=1
+
+print(a)
+
+#REPL -Read Eval Print Loop
+#exit()
+
+
+a=2 # int - integer number
+b=2.3 # float - floating point number
+
+print(a)
+print(b)
+
+
+a= int ('2') # cast/casting
+b=float ('0.5')
+
+print(a,b)
+
+print('Арифметические операции')
+print(a*b)
+print(a/b)
+print(a+b)
+print(a-b)
+
+if 5 % 2 == 0:
+    print('четное число')
+else:
+    print('нечетное число')
+
+
