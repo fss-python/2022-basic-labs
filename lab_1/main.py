d96f54833fadd5e7eace8506f23c48d9dd2c8770
IrinaSozinova <eiole@yandex.ru>
2022-03-11 09:58:55 +0300

    step 2

--- lab_1/main.py
+++ lab_1/main.py
@@ -30,7 +30,7 @@ def read_ecg_raw_file(file_path: Path):
 # Lab 1 implementation goes below
 def calculate_threshold(signal: list):
     """Calculating threshold for RR peaks detection"""
-    threshold=max(ecg_raw) * 0.8
+    threshold=int (max(ecg_raw) * 0.8)
     return (threshold)
     pass
 
@@ -43,12 +43,11 @@ def detect_maximums(signal: list, threshold: int):
             ecg_maximums.append(0)
         elif ecg_raw[i+1]<ecg_raw[i] and ecg_raw[i-1]<ecg_raw[i]:
             ecg_maximums.append(1)
-
         else:
             ecg_maximums.append(0)
 
-
     return (ecg_maximums)
+
     pass
 
 
